93eef9756414ab0c89d73ee31a8365fceb95bdb2
Olivier Dormond <olivier.dormond@gmail.com>
2019-12-02 20:52:05 +0100

    Day 2

---
+++ 2019/2/two.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+from itertools import product
+
+import advent_of_code as adv
+
+
+class Halt(Exception):
+    pass
+
+
+class Computer:
+    def __init__(self, memory):
+        self.memory = memory
+        self.pc = 0
+
+    def op_1(self):
+        """add
+
+        *(pc+3) = *(pc+1) + *(pc+2)
+        """
+        a = self.indirect_load(1)
+        b = self.indirect_load(2)
+        self.indirect_save(a + b, 3)
+        return 4
+
+    def op_2(self):
+        """mul
+
+        *(pc+3) = *(pc+1) * *(pc+2)
+        """
+        a = self.indirect_load(1)
+        b = self.indirect_load(2)
+        self.indirect_save(a * b, 3)
+        return 4
+
+    def op_99(self):
+        raise Halt
+
+    def indirect_load(self, offset):
+        return self.memory[self.memory[self.pc+offset]]
+
+    def indirect_save(self, value, offset):
+        self.memory[self.memory[self.pc+offset]] = value
+
+    def run(self):
+        try:
+            while True:
+                op = self.memory[self.pc]
+                self.pc += getattr(self, f'op_{op}')()
+        except Halt:
+            pass
+
+
+for program, result in (
+    ([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50], [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]),
+    ([1, 0, 0, 0, 99], [2, 0, 0, 0, 99]),
+    ([2, 3, 0, 3, 99], [2, 3, 0, 6, 99]),
+    ([2, 4, 4, 5, 99, 0], [2, 4, 4, 5, 99, 9801]),
+    ([1, 1, 1, 4, 99, 5, 6, 0, 99], [30, 1, 1, 4, 2, 5, 6, 0, 99]),
+):
+    computer = Computer(program)
+    computer.run()
+    assert computer.memory == result
+
+
+program = adv.input(2, adv.to_list_of_int)
+computer = Computer(program[0:1] + [12, 2] + program[3:])
+computer.run()
+
+print("Part one:", computer.memory[0])
+
+
+target_value = 19690720
+for noun, verb in product(range(100), repeat=2):
+    computer = Computer(program[0:1] + [noun, verb] + program[3:])
+    computer.run()
+    if computer.memory[0] == target_value:
+        print("Part two:", 100 * noun + verb)
