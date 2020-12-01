83136cab36ea0a08cb7ce52cc2d8bc4345309b99
Olivier Dormond <olivier.dormond@gmail.com>
2020-12-01 20:35:25 +0100

    Initial setup for 2020

---
+++ 2020/advent_of_code/advent_of_code.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import requests
+
+
+BASE_URL = "https://adventofcode.com/2020/day/"
+with (Path(__file__).parents[1] / "session.cookie").open() as cookie:
+    COOKIES = dict(session=cookie.read().strip())
+
+
+def input(day, converter):
+    return converter(requests.get(BASE_URL + f"{day}/input", cookies=COOKIES).text)
+
+
+def to_list_of_int(text):
+    text = text.strip()
+    return [int(i) for i in (text.split(",") if "," in text else text.splitlines())]
