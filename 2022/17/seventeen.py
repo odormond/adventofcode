4fcacb1251b6c042e3668358fa85daf37f4d4755
Olivier Dormond <olivier.dormond@pix4d.com>
2022-11-29 14:58:46 +0100

    Get ready for 2022

---
+++ 2022/17/seventeen.py
@@ -0,0 +1,3 @@
+#! /usr/bin/env python
+
+import advent_of_code as adv
---
+++ 2022/advent_of_code/advent_of_code.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import sys
+
+import requests
+
+
+BASE_URL = "https://adventofcode.com/2022/day/"
+with (Path(__file__).parents[1] / "session.cookie").open() as cookie:
+    COOKIES = dict(session=cookie.read().strip())
+
+
+def input(converter):
+    day = Path(sys.argv[0]).resolve().parent.name
+    cache = Path('data.txt')
+    if cache.exists():
+        with cache.open() as cache:
+            return converter(cache.read())
+    else:
+        with cache.open('w') as cache:
+            data = requests.get(BASE_URL + f"{day}/input", cookies=COOKIES).text
+            cache.write(data)
+        return converter(data)
+
+
+def to_list_of_int(text):
+    text = text.strip()
+    return [int(i) for i in (text.split(",") if "," in text else text.splitlines())]
+
+
+def to_list_of_str(text):
+    return text.splitlines()
