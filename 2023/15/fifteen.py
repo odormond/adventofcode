84c9a397535e0415af8b9f3739969895343cbee0
Olivier Dormond <olivier.dormond@gmail.com>
2023-12-15 08:06:32 +0100

    Day 15

--- 2023/15/fifteen.py
+++ 2023/15/fifteen.py
@@ -2,24 +2,51 @@
 
 import advent_of_code as adv
 
-test_data = """\
-"""
+test_data = """rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"""
 
 def parse(data):
-    return data
+    return data.replace('\n', '').split(',')
 
 
 def part1(data):
-    return parse(data)
+    return sum(HASH(instruction) for instruction in parse(data))
 
 
-assert (result := part1(test_data)) == "", f"{result=}"
+def HASH(string):
+    h = 0
+    for c in string:
+        h += ord(c)
+        h *= 17
+        h %= 256
+    return h
+
+assert HASH('HASH') == 52
+
+assert (result := part1(test_data)) == 1320, f"{result=}"
 print("Part 1:", part1(adv.input()))
 
 
 def part2(data):
-    return parse(data)
+    boxes = [{} for i in range(256)]
+    for instruction in parse(data):
+        if '=' in instruction:
+            op = '='
+            label, arg = instruction.split('=')
+            box = HASH(label)
+            boxes[box][label] = int(arg)
+        else:
+            op = '-'
+            label, arg = instruction.split('-')
+            box = HASH(label)
+            if label in boxes[box]:
+                boxes[box].pop(label)
+    return sum(
+        i * j * f
+        for i, box in enumerate(boxes, 1)
+        for j, f in enumerate(box.values(), 1)
+    )
 
 
-assert (result := part2(test_data)) == "", f"{result=}"
+
+assert (result := part2(test_data)) == 145, f"{result=}"
 print("Part 2:", part2(adv.input()))
