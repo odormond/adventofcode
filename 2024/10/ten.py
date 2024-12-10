ce37f98f5ddcc6ad1b0c40721569d03a2ac2283f
Olivier Dormond <olivier.dormond@pix4d.com>
2024-12-10 07:27:28 +0100

    Day 10

--- 2024/10/ten.py
+++ 2024/10/ten.py
@@ -1,25 +1,67 @@
 #! /usr/bin/env python
 
+from collections import defaultdict
+
 import advent_of_code as aoc
 
 test_data = """\
+89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732
 """
 
 def parse(data):
-    return data
+    return defaultdict(
+        lambda: -1,
+        {(l, c): int(h) for l, line in enumerate(data.splitlines()) for c, h in enumerate(line)},
+    )
 
 
-def part1(data):
-    return parse(data)
+def neighbour(l, c):
+    return [(l-1, c), (l, c-1), (l+1, c), (l, c+1)]
 
 
-assert (result := part1(test_data)) == "", f"{result=}"
-print("Part 1:", part1(aoc.input()))
+def next_pos(pos, height_map):
+    h = height_map[pos]
+    for n in neighbour(*pos):
+        if height_map[n] == h + 1:
+            yield n
 
 
-def part2(data):
-    return parse(data)
+def trails(start, height_map):
+    paths = [[start]]
+    while paths:
+        path = paths.pop(0)
+        for pos in next_pos(path[-1], height_map):
+            if height_map[pos] == 9:
+                yield path + [pos]
+            else:
+                paths.append(path + [pos])
 
 
-assert (result := part2(test_data)) == "", f"{result=}"
-print("Part 2:", part2(aoc.input()))
+def solve(data):
+    height_map = parse(data)
+    trailheads = [pos for pos, h in height_map.items() if h == 0]
+    total_score = 0
+    total_rating = 0
+    for head in trailheads:
+        summits = set()
+        for trail in trails(head, height_map):
+            summits.add(trail[-1])
+            total_rating += 1
+        total_score += len(summits)
+    return total_score, total_rating
+
+
+test_score, test_rating = solve(test_data)
+assert test_score == 36, f"{test_score=}"
+assert test_rating == 81, f"{test_rating=}"
+
+score, rating = solve(aoc.input())
+print("Part 1:", score)
+print("Part 2:", rating)
