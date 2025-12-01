576f3af3d794d1a382c084fdb68f90a6cb37ac55
Olivier Dormond <olivier.dormond@gmail.com>
2025-12-01 18:12:58 +0100

    Setup 2025

---
+++ 2025/4/four.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import advent_of_code as aoc
+
+test_data = """\
+"""
+
+def parse(data):
+    return data
+
+
+def part1(data):
+    return parse(data)
+
+
+assert (result := part1(test_data)) == "", f"{result=}"
+print("Part 1:", part1(aoc.input()))
+
+
+def part2(data):
+    return parse(data)
+
+
+assert (result := part2(test_data)) == "", f"{result=}"
+print("Part 2:", part2(aoc.input()))
