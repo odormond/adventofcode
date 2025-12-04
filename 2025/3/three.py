01733db713105f41ce3bda11906684e689ebb866
Olivier Dormond <olivier.dormond@gmail.com>
2025-12-04 13:13:34 +0100

    Day 3

--- 2025/3/three.py
+++ 2025/3/three.py
@@ -3,23 +3,33 @@
 import advent_of_code as aoc
 
 test_data = """\
+987654321111111
+811111111111119
+234234234234278
+818181911112111
 """
 
 def parse(data):
-    return data
+    return [[int(b) for b in bank] for bank in data.splitlines()]
 
 
-def part1(data):
-    return parse(data)
+def solver(data, count=2):
+    total = 0
+    for bank in parse(data):
+        batteries = []
+        start_idx = 0
+        for i in range(count):
+            end_idx = len(bank) - count + i + 1
+            hi = max(bank[start_idx:end_idx])
+            start_idx = bank[:end_idx].index(hi, start_idx) + 1
+            batteries.append(hi)
+        total += sum(b * 10**i for i, b in enumerate(reversed(batteries)))
+    return total
 
 
-assert (result := part1(test_data)) == "", f"{result=}"
-print("Part 1:", part1(aoc.input()))
+assert (result := solver(test_data)) == 357, f"{result=}"
+print("Part 1:", solver(aoc.input()))
 
 
-def part2(data):
-    return parse(data)
-
-
-assert (result := part2(test_data)) == "", f"{result=}"
-print("Part 2:", part2(aoc.input()))
+assert (result := solver(test_data, 12)) == 3121910778619, f"{result=}"
+print("Part 2:", solver(aoc.input(), 12))
